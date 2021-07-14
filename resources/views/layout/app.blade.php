09389f42f62b5bcfd3089f70e6e01aaaf052c175
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-14 15:48:23 +0200

    add links

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,8 +3,19 @@
 
 
 <body>
-       
-    @yield('content')
+    {{-- site header --}}
+    <header>
+        {{-- site nav --}}
+        <nav>
+            <a href=" {{route('home')}} "> Home </a>
+            <a href=" {{route('comics.index')}} "> All Comics </a>
+        </nav>
+    </header>
+
+    {{-- site main --}}
+    <main>
+        @yield('content')
+    </main>
 
 </body>
 </html>
