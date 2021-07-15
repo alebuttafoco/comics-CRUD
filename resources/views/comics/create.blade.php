37eed524e9103dae75bc39f99b9ae45e2004d816
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-15 14:56:36 +0200

    add form to create new item

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,8 +1,51 @@
 @extends('layout.app')
 @section('content')
 
+<div class="comics_create">
+    <h1>CREATE NEW COMIC</h1>
+     <div class="container">
+ 
+        {{-- FORM --}}
+        <form action=" {{route('comics.store')}} " method="POST">
+            @csrf
 
-<h1>CREATE NEW COMIC</h1>
+            {{-- path --}}
+            <div class="form-group">
+                <label for="path">Immagine</label>
+                <input type="url" name="path" id="path" class="form-control" placeholder="inserisci l'URL dell'immagine" aria-describedby="titleHelper">
+            </div>
+            {{-- title --}}
+            <div class="form-group">
+                <label for="title">Titolo</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="inserisci il titolo" aria-describedby="titleHelper">
+            </div>
+
+            {{-- description --}}
+            <div class="form-group">
+                <label for="description">Descrizione</label>
+                <textarea name="description" id="description" cols="30" rows="10" placeholder="inserisci la descrizione"></textarea>
+            </div>
+
+            {{-- year --}}
+            <div class="form-group">
+                <label for="year">Anno</label>
+                <input type="number" name="year" id="year" class="form-control" placeholder="inserisci l'anno" aria-describedby="titleHelper">
+            </div>
+
+            {{-- price --}}
+            <div class="form-group">
+                <label for="price">Prezzo</label>
+                <input type="number" name="price" step="0.01" id="price" class="form-control" placeholder="inserisci il prezzo" aria-describedby="titleHelper">
+            </div>
+        
+
+            <button type="submit">Submit</button>
+        
+        </form>
+        
+
+    </div>
+</div>
 
 
 @endsection
