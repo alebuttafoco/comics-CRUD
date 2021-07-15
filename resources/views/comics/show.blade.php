c8fd08f9a188122c35d863148e8f821805ac2ff2
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-15 16:34:43 +0200

    complete style comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,27 +1,28 @@
 @extends('layout.app')
 @section('content')
 
-<div class="comic">
-    <div class="container">
+<div class="comic container-sm">
+    {{-- path --}}
+    <div class="path">
         <img src=" {{$comic->path}} " alt="">
+    </div>
         
-        <h1> {{$comic->title}} </h1>
-        <p> {{$comic->year}} </p>
-        <p> {{$comic->description}} </p>
-        <p> {{$comic->price}} </p>
+    {{-- details --}}
+    <div class="details">
+        <h2 class="comic_title"> {{$comic->title}} </h2>
+        <p class="description"> {{$comic->description}} </p>
+        <span class="price"> {{$comic->price}} € </span>
+    </div>
         
-
-        
-        <form action=" {{route('comics.destroy', $comic->id)}} " method="post">
+        {{-- <form action=" {{route('comics.destroy', $comic->id)}} " method="post">
             @csrf
             @method('DELETE')
             
             <button type="submit">Delete</button>
         </form>
 
-        <a href=" {{route('comics.edit', $comic->id)}} "><button>Edit</button></a>
-    </div>
-        
+        <a href=" {{route('comics.edit', $comic->id)}} "><button>Edit</button></a> --}}
+    
 </div>
 
 
