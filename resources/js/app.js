8905fccf459f6c6524eb9c986e98154b336015fd
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-15 20:11:02 +0200

    not complete js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+
+    }, 
+
+    methods: {
+        alert(){
+            if (!confirm('Vuoi canellarlo definitivamente?')) {
+                returnToPreviousPage();
+            }
+        }
+    }
+})
