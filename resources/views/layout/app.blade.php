19ff29bba153abc651f35895c9a7e7072a93694f
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-16 17:39:50 +0200

    change link order

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,8 +7,8 @@
     <header>
         {{-- site nav --}}
         <nav class="container u-center">
-            <a href=" {{route('home')}} " class=" {{Route::currentRouteName() == 'home' ? 'active_link' : ''}} "> Home </a>
             <a href=" {{route('comics.index')}} " class="{{Route::currentRouteName() == 'comics.index' ? 'active_link' : ''}}"> Comics </a>
+            <a href=" {{route('home')}} " class=" {{Route::currentRouteName() == 'home' ? 'active_link' : ''}} "> Admin </a>
             <a href=" {{route('comics.create')}} " class="{{Route::currentRouteName() == 'comics.create' ? 'active_link' : ''}}"> Create </a>
         </nav>
     </header>
